be301b1dd71524c5cf89f0392bf2ed41f0476037
AlecSimoneau <simoneau.alec@gmail.com>
2025-04-10 10:22:47 -0400

    Update openCV_Testing.py
    Changing the name to VidFrameClass, and updating some changes, not the final version, but showing an intermediary step

--- openCV_Testing.py
+++ openCV_Testing.py
@@ -1,52 +1,87 @@
-## This file is a test to see if i can create multiple images, store them, and play them as a video
 import cv2 as cv
 import numpy as np
+import keyboard as kb
+## VARS ##
 
+#temp iteratable
+j = 1 
 
-## I want to make a class to assign the same attributes to each window
-## The attributes i want to assign are width, height and channels
-## I'm not sure the best way to do this so...
-class VidFrame:
-    
-    ## I tried __init__ but it didn't work the way i wanted?
+pixX = None
+pixY = None
+pixPosition = [pixX,pixY]
+oldSize = (127,127)
+##########
 
-    #def __init__(self, height, width, channels,frame):
-    #self.height = height
-    #self.width = width
-    #self.channels = channels
-    #self.frame - frame
-        
-    ## I'm using the library openCV to create the images, and I want to assign an object the atribute frame,
-    ## and have the frame have atributes height, width and channels
-    def frame(self,height = 15,width = 15,channels = 3):
+class VidFrame():
+
+    #Initializes with height, width and color channels 
+    def __init__(self, height = oldSize[0], width = oldSize[1], channels = 3,pixX, pixY, pixPosition):
         self.height = height
         self.width = width
         self.channels = channels
-
+        self.pixX = pixX
+        self.pixY = pixY
+        pixPosition = [pixX,pixY]
+    
+    # this should make a black image with the atributes height, width and channels
+    def makeFrame(self):
+        im = np.zeros((self.height,self.width,self.channels),np.uint8)    
+        return im
+                
     ## This is to scale up the image, to reduce the initail amount of pixels i work with
     ## It works so thats good
-    def upScale(scaleFactor,frame):
-        bigFrame = cv.resize(frame,(frame.height * scaleFactor, frame.width * scaleFactor))
+    def upScale(self,scaleFactor,name):
+        if scaleFactor > 0:
+            self.height = self.height * scaleFactor
+            self.width = self.width * scaleFactor
+            return cv.resize(name,(self.height, self.width),interpolation = cv.INTER_LINEAR_EXACT)
+        elif scaleFactor < 0:
+            self.height = self.height // (scaleFactor * -1)
+            self.width = self.width // (scaleFactor * -1)
+            return cv.resize(name,(self.height, self.width),interpolation = cv.INTER_LINEAR_EXACT)
+        else:
+            return name
+        
 
     ## This just makes a window with a 'title' and puts the image 'frame' in it
-    def show(title, frame):
-        cv.imshow(title, VidFrame.frame)
-        # Keeps the window until you press a key
-        cv.waitKey(0)
+    def show(self,title: str,name):#,delay):
+        cv.imshow(title,name)
+    
+    
+    ### When called I want the following four functions to update the pixel position to one pixel adjacent to the original position
+    def moveRight(self,im,pixPositon):
 
-# assigns p1 to the VidFrame class
-p1 = VidFrame()
+        pixPositionOld = pixPosition
+        pixPositionNew = [pixPosition[0]+1,pixPosition[1]]
+        im[pixPositionNew] = [255,255,255]
+        im[pixPositionOld] = [0,0,0]
+        print("Moved Right!")
 
-##### This makes an image but here is where it breaks
-## the p1.frame.xxxx doesn't work, it says that frame doesn't have the atribute height?
-## I just need to recover height, width and channels from def frame(), but there's probably a better way to do this
+    def moveLeft(self,im):
+        pixPositionOld = pixPosition
+        pixPositionNew = [pixX-1,pixY]
+        im[pixPositionNew] = [255,255,255]        
+        print("Moved Left!")
+        
+    def moveUp(self,im):
+        pixPositionOld = pixPosition
+        pixPositionNew = [pixX,pixY+1]
+        im[pixPositionNew] = [255,255,255]
+        print("Moved Up!")
+        
+    def moveDown(self,im):
+        pixPositionOld = pixPosition
+        pixPositionNew = [pixX,pixY-1]
+        im[pixPositionNew] = [255,255,255]
+        print("Moved Down!")
+    ###
+    
+    ## This is where I might center the pixel, but I want to solve the above functions first
+    def center(image):
+        pass
 
-p1.frame() = np.zeros((p1.frame.height,p1.frame.width,p1.frame.channels),np.uint8)
 
-####
+# Move logic
 
-# Makes the image bigger
-p1.upScale(10,p1.frame)
-
-# Calls the function to create the window
-p1.show("Title",p1.frame)
+# pixel position
+# move -> on([x + a , y + b]) = [x_new,y_new] & delete([x_old,y_old])
